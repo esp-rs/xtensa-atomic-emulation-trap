055bf4d09f9c6863968c0317bb46b3469013b4ec
Scott Mabin <scott@mabez.dev>
2022-05-03 10:03:29 +0200

    Atomic emulation working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-#![feature(asm_experimental_arch)]
 
 use xtensa_lx_rt::exception::{Context, ExceptionCause};
 
@@ -20,11 +19,17 @@ use core::fmt::Write;
 unsafe fn __exception(cause: ExceptionCause, save_frame: &mut Context) {
     match cause {
         ExceptionCause::Illegal if atomic_emulation(save_frame) => {
-            save_frame.PC += core::mem::size_of::<usize>() as u32;
+            save_frame.PC += 3; // 24bit instructions
+            return;
         },
         _ => {
-            todo!("Allow a custom execption forwarding here")
+            // todo!("Allow a custom execption forwarding here")
         }
+    }
+
+    write!(Uart, "ERROR unrecoverable exception").ok();
+
+    loop {
     }
 }
 
@@ -37,14 +42,13 @@ pub fn test_print() {
 }
 
 #[link_section = ".rwtext"]
-pub unsafe fn atomic_emulation(save_frame: &Context) -> bool {
-    // writeln!(Uart, "About to deref = {}", save_frame.PC).ok();
+pub unsafe fn atomic_emulation(save_frame: &mut Context) -> bool {
+    writeln!(Uart, "About to deref = {}", save_frame.PC).ok();
     // deref the addr to find the instruction we trapped on.
-    // because the data is in the cache, we need to use special instruction to read from the cache memories
    
-    let insn: usize;
     let pc = save_frame.PC;
 
+    // if the PC address isn't word aligned, we need to read two words and capture the relevant instruction
     let insn = if pc % 4 != 0 {
         let prev_aligned = pc & !0x3;
         let offset = (pc - prev_aligned) as usize;
@@ -63,7 +67,7 @@ pub unsafe fn atomic_emulation(save_frame: &Context) -> bool {
     if (insn & 0b11111111_000000000000_1111) == 0b00010011_000000000000_0000 {
         let target = (insn >> 4) & 0b1111;
         let sr = (insn >> 8) & 0b11111111;
-        writeln!(Uart, "Emulating WSR, target reg = {}, special reg = {}, value = {}", target, sr, register_value_from_index(target, save_frame)).ok();
+        // writeln!(Uart, "Emulating WSR, target reg = {}, special reg = {}, value = {}", target, sr, register_value_from_index(target, save_frame)).ok();
         if sr == SCOMPARE1_SR { // is the dest register SCOMPARE1
             // write the source _value_ into our emulated SCOMPARE1
             let target_value = register_value_from_index(target, save_frame);
@@ -72,14 +76,34 @@ pub unsafe fn atomic_emulation(save_frame: &Context) -> bool {
             return true
         }
     }
-        
 
     // next check, is it the S32C1I instruction? RRI8 Format
-    if (insn & 0b1111_00000000_1111) == 0b1110_00000000_0011 {
+    if (insn & 0b1111_00000000_1111) == 0b1110_00000000_0010 {
         let reg_mask = 0b1111;
-        let dest = (insn >> 4) & reg_mask;
+        let target = (insn >> 4) & reg_mask;
         let source = (insn >> 8) & reg_mask;
         let offset = (insn >> 16) & 0b11111111;
+
+        let target_value = register_value_from_index(target, save_frame);
+        let source_value = register_value_from_index(source, save_frame);
+        // let source_address = source_value + ((offset as u32) << 2);
+
+        let source_address = source_value; // TODO use offset when not zero
+
+        // writeln!(Uart, "E S32, target = {}, source = {}, offset = {}", target_value, source_value, offset).ok();
+
+        let memory_value = *(source_address as *const u32);
+
+        writeln!(Uart, "E S32, mem value = {}, scompare: {}", memory_value, SCOMPARE1).ok();
+
+        if memory_value == SCOMPARE1 {
+            *(source_address as *mut u32) = target_value;
+        }
+
+        let target_value_mut = register_value_mut_from_index(target, save_frame);
+        *target_value_mut = memory_value;
+
+        return true;
     }
 
     false
@@ -122,20 +146,24 @@ fn register_value_from_index(index: usize, save_frame: &Context) -> u32{
     }
 }
 
-// #[cfg(test)]
-// mod test_super {
-//     use super::*;
-
-//     const WSR_A0_SCOMPARE1: u32 = 0x000c13;
-
-//     #[test]
-//     fn test_wsr_emulation() {
-//         let instruction_memory = WSR_A0_SCOMPARE1; // asm!("wsr a0, SCOMPARE1")
-
-//         let context = Context {
-//             PC: &instruction_memory as *const _ as _, // address of "instruction_memory" aka where our program halted
-//             ..Default::default()
-//         };
-        
-//     }
-// }
+fn register_value_mut_from_index(index: usize, save_frame: &mut Context) -> &mut u32 {
+    match index {
+        0 => &mut save_frame.A0,
+        1 => &mut save_frame.A1,
+        2 => &mut save_frame.A2,
+        3 => &mut save_frame.A3,
+        4 => &mut save_frame.A4,
+        5 => &mut save_frame.A5,
+        6 => &mut save_frame.A6,
+        7 => &mut save_frame.A7,
+        8 => &mut save_frame.A8,
+        9 => &mut save_frame.A9,
+        10 => &mut save_frame.A10,
+        11 => &mut save_frame.A11,
+        12 => &mut save_frame.A12,
+        13 => &mut save_frame.A13,
+        14 => &mut save_frame.A14,
+        15 => &mut save_frame.A15,
+        _ => unreachable!()
+    }
+}
